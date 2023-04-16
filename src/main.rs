b40c65257358be685809de0f8fad96ffd10913e9
n1cOV34 <nvelasquez72@gmail.com>
2023-04-16 17:10:10 -0500

    main rust con las lineas de aprendizaje

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut a = vec!["test","test2"];
+    a.push("fruta");    
+    println!("{:?}",a);//Opcion 1 Impresion de un vector
+    println!("{a:?}");//Opcion 2 Impresion de un vector
+     
+}
